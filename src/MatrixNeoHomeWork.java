4401165ed45e144f3c67f57a7e14cef379ecb1a4
Serhii933 <Subota933@gmail.com>
2023-11-01 08:03:33 +0300

    HomeWork reply

--- src/MatrixNeoHomeWork.java
+++ src/MatrixNeoHomeWork.java
@@ -1,9 +1,22 @@
-import java.util.Random;
-
 public class MatrixNeoHomeWork {
     public static void main(String[] args) {
-        int[][] trainingMatrix = MatrixGenerator.generateTrainingMatrix();
-        MatrixPrinter.printMatrix(trainingMatrix);
+            int[][] matrix = new int[8][8];
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (i % 2 == 0) {
+                        matrix[i][j] = j + 1;
+                    } else {
+                        matrix[i][j] = 8 - j;
+                    }
+                }
+            }
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
     }
-}
+
 
